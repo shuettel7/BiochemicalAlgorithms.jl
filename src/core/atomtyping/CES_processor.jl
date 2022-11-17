89eeddbd15b0e507b0fa62e28f32d8688e1f9e44
shuettel7 <shuettel@students.uni-mainz.de>
2022-11-17 17:57:42 +0100

    File setup and planning

---
+++ src/core/atomtyping/CES_processor.jl
@@ -0,0 +1,13 @@
+using BiochemicalAlgorithms
+
+
+function CES_parser()
+    # check grammar of current top position in DEF
+    # build string with same grammar for current atom atom/molecule information
+    # return bool, move to next position in DEF file or end search on positive 
+end
+
+function CES_processor()
+    # use DEF file and information from CES_parser process to cycle through
+    # DEF file until Atomtype is assigned or no more criteria fit (return error?)
+end
