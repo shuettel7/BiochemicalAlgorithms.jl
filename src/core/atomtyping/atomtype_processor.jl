8e51667b32c09646620e9bda0a1a1f50e7917544
shuettel7 <shuettel@students.uni-mainz.de>
2022-11-23 13:56:54 +0100

    filtering of DEF dataframe updated
    for atomic_number, num_neighbors, num_H_bonds, and electron_withdrawal_groups

--- src/core/atomtyping/atomtype_processor.jl
+++ src/core/atomtyping/atomtype_processor.jl
@@ -1,48 +1,72 @@
 using BiochemicalAlgorithms
 
+export get_molecule_atomtypes!, count_EWG
+
 function get_molecule_atomtypes!(mol::AbstractMolecule, mapfile::AbstractString)
     PreprocessingMolecule!(mol)
-    def_file = load_atomtyping_DEF(mapfile)
+    atmprops_df = create_atom_preprocessing_df(mol)
+    def_file_df = load_atomtyping_DEF(mapfile)
     
     # loop over atoms, prefilter dataframe by element and neighbor count
     for i = (1:nrow(mol.atoms))
-        df_curr = copy(def_file)
-        EWG_groups = -1
+        def_curr_df = copy(def_file_df)
+        default_num_H_neighbors = -1
+        num_H_neighbors = countmap(in(["H1"]).(atmprops_df.ElementWithNeighborCount[atmprops_df.Neighbors[i]]))[true]
+        num_EWG_groups = default_num_EWG_groups = -1
         if Int(mol.atoms.element[i]) == 1
-            EWG_groups = count_EWG(i, mol)
+            EWG_groups = count_EWG(i, mol, atmprops_df)
         end
-        if !isempty(df_curr[(df_curr[!,:atomic_number] .== Int(mol.atoms.element[i])), :])
-            df_curr = df_curr[(df_curr[!,:atomic_number] .== Int(mol.atoms.element[i])), :]
+        ### filter DEF file dataframe by atom properties from preprocessing of molecule
+        # filter current dataframe def_curr_df by element number
+        if !isempty(def_curr_df[(def_curr_df[!,:atomic_number] .== Int(mol.atoms.element[i])), :])
+            df_curr = def_curr_df[(def_curr_df[!,:atomic_number] .== Int(mol.atoms.element[i])), :]
         end
-        if !isempty(df_curr[(df_curr[!,:num_neighbors] .== lastindex(neighbors(mol.properties["mol_graph"], i))), :])
-            df_curr = df_curr[(df_curr[!,:num_neighbors] .== lastindex(neighbors(mol.properties["mol_graph"], i))), :]
-        end
-        if !isempty(df_curr[(df_curr[!,:num_H_bonds] .== neighbors(mol.properties[i]["mol_graph"], i)), :])
 
+        # filter current dataframe def_curr_df by number of neighbors
+        if !isempty(df_curr[(df_curr[!,:num_neighbors] .== lastindex(atmprops_df.Neighbors[i])), :])
+            df_curr = df_curr[(df_curr[!,:num_neighbors] .== lastindex(atmprops_df.Neighbors[i])), :]
         end
-        df_curr = df_curr[(df_curr[!,:atomic_number] .== Int(mol.atoms.element[i])) .& (df_curr[!,:num_neighbors] .== Int(mol.atoms.num_neighbors[i])),:]
 
+        # filter current dataframe def_curr_df by number of hydrogens in direct neighborhood
+        if !isempty(df_curr[(df_curr[!,:num_H_bonds] .== num_H_neighbors), :])
+            df_curr = df_curr[(df_curr[!,:num_H_bonds] .== num_H_neighbors), :]
+        else
+            if !isempty(df_curr[(df_curr[!,:num_H_bonds] .== default_num_H_neighbors), :])
+                df_curr = df_curr[(df_curr[!,:num_H_bonds] .== default_num_H_neighbors), :]
+            end
+        end
+
+        # filter current dataframe def_curr_df by number of electron-withdrawal groups
+        if !isempty(df_curr[(df_curr[!,:electron_withdrawal_groups] .== num_EWG_groups), :])
+            df_curr = df_curr[(df_curr[!,:electron_withdrawal_groups] .== num_EWG_groups), :]
+        else
+            if !isempty(df_curr[(df_curr[!,:electron_withdrawal_groups] .== default_num_EWG_groups), :])
+                df_curr = df_curr[(df_curr[!,:electron_withdrawal_groups] .== default_num_EWG_groups), :]
+            end
+        end
     end
 end
 
 
-function count_EWG(num::Int, mol::AbstractMolecule)
-    # Electron withdrawal Atoms according to antechamber document are only: N, O, F, Cl, and Br
+function count_EWG(num::Int, mol::AbstractMolecule, atmprops_df::DataFrame)
+    # usually only used on hydrogen atoms:
+    # Electron withdrawal Atoms according to antechamber document are: N, O, F, Cl, and Br
+    # which are bound to the immediate neighbour
     # To Do: Test differences for Atom Typing, see below typical know EWG
     strong_pullers = ["Cl1", "F1", "Br1", "I1", "O1", "S1"]
     possible_pullers = ["C2", "C3", "C4", "S3", "N3", "P3", "P4", "O2", "S2"]
     elec_pullers_num = 0
-    if true in in(mol.atoms.properties[num]["ElementWithNeighborCount"]).(possible_pullers)
-        for i = (1:lastindex(neighbors(mol.properties["mol_graph"], num)))
-            if true in in(strong_pullers).(ATD_df.Element_wNeighborCount[ATD_df.Secondary_Neighbors[num][i]])
+    if true in in(atmprops_df.ElementWithNeighborCount[atmprops_df.Neighbors[num]]).(possible_pullers)
+        for (i, neigh) in enumerate(atmprops_df.Neighbors[num])
+            if true in in(strong_pullers).(atmprops_df.ElementWithNeighborCount[atmprops_df.SecondaryNeighbors[num][i]]) &&
+                atmprops_df.ElementWithNeighborCount[neigh] in possible_pullers
                 # all neighbors of neighbor that are in strong_pullers are an EWG
-                elec_pullers_num += countmap(in(strong_pullers).(mol.atoms.elem[ATD_df.Secondary_Neighbors[num][i]]))[true]
+                elec_pullers_num += countmap(in(strong_pullers).(atmprops_df.ElementWithNeighborCount[atmprops_df.SecondaryNeighbors[num][i]]))[true]
             end
         end
     end
     if elec_pullers_num == 0
         elec_pullers_num = -1
     end
-    # # println("EWG: ", elec_pullers_num)
     return elec_pullers_num
 end
