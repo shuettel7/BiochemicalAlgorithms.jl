6e34430b2d732a9f451db5a755616589f1b37c38
Samuel Hüttel <shuettel@students.uni-mainz.de>
2022-11-24 22:23:13 +0100

    adapting with DataFramesMeta in Preprocessing and ClearPreprocessing

--- src/core/atomtyping/preprocessing_molecule.jl
+++ src/core/atomtyping/preprocessing_molecule.jl
@@ -1,5 +1,5 @@
 using BiochemicalAlgorithms
-using Graphs, SimpleWeightedGraphs, StatsBase, EnumX
+using Graphs, SimpleWeightedGraphs, StatsBase, EnumX, DataFramesMeta
 
 export PreprocessingMolecule!, ClearPreprocessingMolecule!, create_atom_preprocessing_df
 
@@ -21,79 +21,75 @@ function PreprocessingMolecule!(mol::AbstractMolecule)
 
     ### Assign properties to Atoms and Bonds
     for (j, sublist) in enumerate(chem_cycle_list)
-        for (k, sublitem) in enumerate(sublist)
-            ### assign "ar" property in mol.bonds.properties["TRIPOS_tag] position of Dictionary
-            if k != 1 
-                if in(["AR1", "AR2", "AR3"]).(ring_class_list[sublitem][1]) &&
-                    in(["AR1", "AR2", "AR3"]).(ring_class_list[sublist[k-1]][1])
-                    bond_row = get_bond_row(mol, sublitem, sublist[k-1])
-                    if bond_row != 0
-                        mol.bonds.properties[bond_row]["TRIPOS_tag"] = "ar"
-                    end
-                end 
-            else
-                if in(["AR1", "AR2", "AR3"]).(ring_class_list[sublitem][1]) &&
-                    in(["AR1", "AR2", "AR3"]).(ring_class_list[sublist[lastindex(sublist)]][1])
-                    bond_row = get_bond_row(mol, sublitem, sublist[lastindex(sublist)])
-                    if bond_row != 0
-                        mol.bonds.properties[bond_row]["TRIPOS_tag"] = "ar"
-                    end
-                end
+        ring_atoms = DataFrame(:number => sublist)
+        ring_bonds = DataFrame(:a1 => sublist, :a2 => append!(sublist[2:lastindex(sublist)], sublist[1]))
+        if all(in(["AR1"]).(ring_class_list[sublist]))
+            ring_bonds = DataFrame(:a1 => sublist, :a2 => append!(sublist[2:lastindex(sublist)], sublist[1]))
+            @with innerjoin(ring_bonds, mol.bonds, on = [:a1, :a2]) begin
+                push!.(:properties, "TRIPOS_tag" => "ar")
             end
-            if !haskey(mol.atoms.properties[sublitem], "CycleListNum")
-                mol.atoms.properties[sublitem]["CycleListNum"] = [j]
-                mol.atoms.properties[sublitem]["CycleSize"] = [lastindex(sublist)]
-            elseif !all(in(mol.atoms.properties[sublitem]["CycleListNum"]).([j]))
-                mol.atoms.properties[sublitem]["CycleListNum"] = append!(mol.atoms.properties[sublitem]["CycleListNum"], [j])
-                mol.atoms.properties[sublitem]["CycleSize"] = append!(mol.atoms.properties[sublitem]["CycleSize"], [lastindex(sublist)])
+            @with innerjoin(ring_bonds, mol.bonds, on = [:a1 => :a2, :a2 => :a1]) begin
+                push!.(:properties, "TRIPOS_tag" => "ar")
             end
+        end
+        @with innerjoin(ring_atoms, mol.atoms, on = [:number]) begin
+                push!.(:properties, "CycleListNum" => [j])
+                push!.(:properties, "CycleSize" => [lastindex(sublist)])
         end
     end
 
-    ElemWNeighbourCount_vector = Vector{String}()
-    for i = (1:nrow(mol.atoms))
-        mol.atoms.properties[i]["ElementWithNeighborCount"] = ElemWNeighbourCount = string(enumToString(mol.atoms.element[i]), lastindex(neighbors(mol_graph, i)))
-        mol.atoms.properties[i]["Neighbors"] = AtomNeighbors = neighbors(mol_graph, i)
-        mol.atoms.properties[i]["SecondaryNeighbors"] = Vector{Vector{Int}}()
-        for (k, neigh) in enumerate(AtomNeighbors)
-            append!(mol.atoms.properties[i]["SecondaryNeighbors"], [[]])
-            for sec_neigh in neighbors(mol_graph, neigh)
-                if sec_neigh != i
-                    push!(mol.atoms.properties[i]["SecondaryNeighbors"][k], sec_neigh)
-                end
-            end
-        end
-        push!(ElemWNeighbourCount_vector, ElemWNeighbourCount)
-        mol.atoms.properties[i]["AromaticityType"] = mol.properties["ring_class_list"][i]
-        for l in values(countmap(wgraph_adj_matrix[i, neighbors(mol.properties["mol_weighted_graph"], i)]))
-            if !haskey(mol.atoms.properties[i], "BondTypes")
-                mol.atoms.properties[i]["BondTypes"] = [enumToString(BondShortOrderType(l))]
-            elseif !all(in(mol.atoms.properties[i]["BondTypes"]).([enumToString(BondShortOrderType(l))]))
-                mol.atoms.properties[i]["BondTypes"] = append!(mol.atoms.properties[i]["BondTypes"], [enumToString(BondShortOrderType(l))])
-            end
-        end
-    end
-    mol.properties["Amide_tag_list"] = amide_list = amide_processor(mol, mol_graph, wgraph_adj_matrix, ElemWNeighbourCount_vector)
+    ElemWNeighbourCount_vector = @with mol.atoms @byrow string(enumToString(:element), lastindex(neighbors(mol_graph, :number)))
+    @with mol.atoms @byrow push!(:properties, 
+                        "ElementWithNeighborCount" => string(enumToString(:element), lastindex(neighbors(mol_graph, :number))),
+                        "Neighbors" => neighbors(mol_graph, :number), 
+                        "AromaticityType" => mol.properties["ring_class_list"][:number],
+                        "SecondaryNeighbors" => secondary_neighbors(mol_graph, :number),
+                        "BondTypes" => bondShortOrder_types(:number, mol_graph, wgraph_adj_matrix))        
+    
+    amide_list = amide_processor(mol, mol_graph, ElemWNeighbourCount_vector)
     if !isempty(amide_list)
-        for atompair in amide_list
-            bond_row = get_bond_row(mol, atompair[1], atompair[2])
-            if bond_row != 0 && !haskey(mol.bonds.properties[bond_row] ,"TRIPOS_tag")
-                mol.bonds.properties[bond_row]["TRIPOS_tag"] = "am"
-            end
+        amide_bonded_atoms_df = DataFrame(NamedTuple{(:a1, :a2)}.(amide_list))
+        @with innerjoin(amide_bonded_atoms_df, mol.bonds, on = [:a1, :a2]) begin
+            push!.(:properties, "TRIPOS_tag" => "am")
         end
-    else
-        delete!(mol.properties, "Amide_tag_list")
+        @with innerjoin(amide_bonded_atoms_df, mol.bonds, on = [:a1 => :a2, :a2 => :a1]) begin
+            push!.(:properties, "TRIPOS_tag" => "am")
+        end
     end
 end
 
 
-function amide_processor(mol::AbstractMolecule, mol_graph::SimpleGraph, wgraph_adj_matrix::Graphs.SparseMatrix, ElementWNeighbourCount_vector::Vector{String})
+function bondShortOrder_types(num::Int, mol_graph::Graph, wgraph_adj_matrix::Graphs.SparseMatrix)
+    BondShortVec = Vector{String}()
+    for i in keys(countmap(wgraph_adj_matrix[num, neighbors(mol_graph, num)]))
+        push!(BondShortVec, enumToString(BondShortOrderType(Int(i))))
+    end
+    return BondShortVec
+end
+
+
+function secondary_neighbors(mol_graph::Graph, num::Int)
+    SecNeighVec = Vector{Vector{Int}}()
+    for (k, neigh) in enumerate(neighbors(mol_graph, num))
+        append!(SecNeighVec, [[]])
+        for sec_neigh in neighbors(mol_graph, neigh)
+            if sec_neigh != num
+                push!(SecNeighVec[k], sec_neigh)
+            end
+        end
+    end
+    return SecNeighVec
+end
+
+
+function amide_processor(mol::AbstractMolecule, mol_graph::Graph, ElementWNeighbourCount_vector::Vector{String})
     amide_bond_vector = Vector{Tuple{Int64, Int64}}()
     nitrogen_atoms_list = mol.atoms[(mol.atoms[!, :element] .== Elements.N), :number]
-    for i in nitrogen_atoms_list
-        for j in neighbors(mol_graph, i)
-            if lastindex(neighbors(mol_graph, j)) > 2 && in(ElementWNeighbourCount_vector[neighbors(mol_graph, j)]).("O1")
-                push!(amide_bond_vector, (j,i))
+    for nitrogen in nitrogen_atoms_list
+        for amide_neigh in neighbors(mol_graph, nitrogen)
+            if lastindex(neighbors(mol_graph, amide_neigh)) > 2 && 
+                in(ElementWNeighbourCount_vector[neighbors(mol_graph, amide_neigh)]).("O1")
+                push!(amide_bond_vector, (amide_neigh,nitrogen))
             end
         end
     end
@@ -102,54 +98,23 @@ end
 
 
 function ClearPreprocessingMolecule!(mol::AbstractMolecule)
-    if haskey(mol.properties, "mol_graph")
-        delete!(mol.properties, "mol_graph")
+    mol_props_names = ["mol_graph", "adjacency_matrix", "mol_weighted_graph", 
+                        "weighted_graph_adj_matrix", "chem_cycle_list", 
+                        "ring_intersections_matrix", "ring_class_list"]
+    atom_props_names = ["CycleListNum", "CycleSize", "ElementWithNeighborCount",
+                        "AromaticityType", "BondTypes", "Neighbors", "SecondaryNeighbors"]
+    bond_props_names = ["TRIPOS_tag"]
+    for name in mol_props_names
+        delete!(mol.properties, name)
     end
-    if haskey(mol.properties, "adjacency_matrix")
-        delete!(mol.properties, "adjacency_matrix")
-    end
-    if haskey(mol.properties, "mol_weighted_graph")
-        delete!(mol.properties, "mol_weighted_graph")
-    end
-    if haskey(mol.properties, "weighted_graph_adj_matrix")
-        delete!(mol.properties, "weighted_graph_adj_matrix")
-    end
-    if haskey(mol.properties, "chem_cycle_list")
-        delete!(mol.properties, "chem_cycle_list")
-    end
-    if haskey(mol.properties, "ring_intersections_matrix")
-        delete!(mol.properties, "ring_intersections_matrix")
-    end
-    if haskey(mol.properties, "ring_class_list")
-        delete!(mol.properties, "ring_class_list")
-    end
-    if haskey(mol.properties, "Amide_tag_list")
-        delete!(mol.properties, "Amide_tag_list")
-    end
-    for i = (1:nrow(mol.atoms))
-        if haskey(mol.atoms.properties[i], "CycleListNum")
-            delete!(mol.atoms.properties[i], "CycleListNum")
-            delete!(mol.atoms.properties[i], "CycleSize")
-        end
-        if haskey(mol.atoms.properties[i], "ElementWithNeighborCount")
-            delete!(mol.atoms.properties[i], "ElementWithNeighborCount")
-        end
-        if haskey(mol.atoms.properties[i], "AromaticityType")
-            delete!(mol.atoms.properties[i], "AromaticityType")
-        end
-        if haskey(mol.atoms.properties[i], "BondTypes")
-            delete!(mol.atoms.properties[i], "BondTypes")
-        end
-        if haskey(mol.atoms.properties[i], "Neighbors")
-            delete!(mol.atoms.properties[i], "Neighbors")
-        end
-        if haskey(mol.atoms.properties[i], "SecondaryNeighbors")
-            delete!(mol.atoms.properties[i], "SecondaryNeighbors")
+    @with mol.atoms begin
+        for name in atom_props_names
+            delete!.(:properties, name)
         end
     end
-    for i = (1:nrow(mol.bonds))
-        if haskey(mol.bonds.properties[i], "TRIPOS_tag")
-            delete!(mol.bonds.properties[i], "TRIPOS_tag")
+    @with mol.bonds begin
+        for name in bond_props_names
+            delete!.(:properties, name)
         end
     end
 end
@@ -165,27 +130,33 @@ function create_atom_preprocessing_df(mol::AbstractMolecule)
 
     col_names = ["AromaticityType", "ElementWithNeighborCount", "Neighbors", "SecondaryNeighbors", 
                  "BondTypes", "CycleSize", "CycleListNum"]
-    atom_props_df = DataFrame([Vector{Vector{String}}(), Vector{String}(), Vector{Vector{Int64}}(), 
+    atom_props_df = DataFrame([Vector{String}(), Vector{String}(), Vector{Vector{Int64}}(), 
                             Vector{Vector{Vector{Int64}}}(), Vector{Vector{String}}(), Vector{Vector{Int64}}(), 
                             Vector{Vector{Int64}}()], col_names)
-    for (k, atm) in enumerate(eachrow(mol.atoms))
-        push!(atom_props_df, (Vector{String}(), "", Vector{Int64}(), 
-                            Vector{Vector{Int64}}(), Vector{String}(), Vector{Int64}(), 
-                            Vector{Int64}()))
-        for col in col_names
-            if haskey(atm.properties, col)
-                atom_props_df[!,col][k] = atm.properties[col]
-            end
-        end
+    mol_atom_props = DataFrame()
+    @with mol.atoms begin
+        push!.(mol_atom_props, :properties)
     end
+    println(mol_atom_props)
+    
+    # for (k, atm) in enumerate(eachrow(mol.atoms))
+    #     push!(atom_props_df, (string(), string(), Vector{Int64}(), 
+    #                         Vector{Vector{Int64}}(), Vector{String}(), Vector{Int64}(), 
+    #                         Vector{Int64}()))
+    #     for col in col_names
+    #         if haskey(atm.properties, col)
+    #             atom_props_df[!,col][k] = atm.properties[col]
+    #         end
+    #     end
+    # end
     return atom_props_df
 end
 
 
 function aromaticity_type_processor(LList::Vector{Vector{Int64}}, wgraph_adj::Graphs.SparseMatrix, inters_matrix::Matrix{Vector{Int64}}, mol::AbstractMolecule)
-    ring_class_list = Vector{Vector{String}}(undef, nrow(mol.atoms))
+    ring_class_list = Vector{String}(undef, nrow(mol.atoms))
     for i = (1:nrow(mol.atoms))
-        ring_class_list[i] = ["NG"]
+        ring_class_list[i] = "NG"
     end
     for (numvlist, vlist) in enumerate(LList)
 
@@ -206,11 +177,11 @@ function aromaticity_type_processor(LList::Vector{Vector{Int64}}, wgraph_adj::Gr
         end
         if (pi_elec / lastindex(vlist)) == 1.0
             for x in vlist
-                ring_class_list[x] = ["AR1"]
+                ring_class_list[x] = "AR1"
             end
         elseif (pi_elec / lastindex(vlist)) > 1/2 && (pi_elec / lastindex(vlist)) <= 1 && ONSP_present && lastindex(vlist) > 4
             for x in vlist
-                ring_class_list[x] = ["AR2"]
+                ring_class_list[x] = "AR2"
             end
         elseif (pi_elec / lastindex(vlist)) > 1/2 && (pi_elec / lastindex(vlist)) < 1 && !ONSP_present && lastindex(vlist) > 4
             # check if Ring vlist has intersections with other rings in molecule and if these are aromatic
@@ -221,8 +192,8 @@ function aromaticity_type_processor(LList::Vector{Vector{Int64}}, wgraph_adj::Gr
                     if countmap(in([BondOrder.Double]).(atom1_bonds.order))[1] == 1 &&
                         countmap(in([BondOrder.Double]).(atom2_bonds.order))[1] == 1
                         for x in vlist
-                            if !in(ring_class_list[x]).("AR1")
-                                ring_class_list[x] = ["AR1"]
+                            if ring_class_list[x] != "AR1"
+                                ring_class_list[x] = "AR1"
                             end
                         end 
                     end           
@@ -230,8 +201,8 @@ function aromaticity_type_processor(LList::Vector{Vector{Int64}}, wgraph_adj::Gr
             end
         elseif (pi_elec / lastindex(vlist)) < 1/2
             for x in vlist
-                if !in(ring_class_list[x]).("AR5") && !in(ring_class_list[x]).("AR1")
-                    ring_class_list[x] = ["AR5"]    
+                if ring_class_list[x] != "AR5" && ring_class_list[x] != "AR1"
+                    ring_class_list[x] = "AR5"  
                 end
             end
         end
@@ -277,16 +248,6 @@ end
 
 function enumToString(AnyEnum::Enum)
     return String(Symbol(AnyEnum))
-end
-
-
-function get_bond_row(mol::AbstractMolecule, atom1::Int, atom2::Int)
-    for i = (1:nrow(mol.bonds))
-        if (mol.bonds.a1[i] == atom1 && mol.bonds.a2[i] == atom2) || (mol.bonds.a1[i] == atom2 && mol.bonds.a2[i] == atom1)
-            return i
-        end
-    end
-    return 0
 end
 
 
