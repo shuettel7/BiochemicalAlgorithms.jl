248db2b8ac63cde9c5b653bd201f35c1e042576a
Samuel Hüttel <shuettel@students.uni-mainz.de>
2022-11-27 01:07:07 +0100

    new atomtyping: match detection from top instead of filtering of df

--- src/core/atomtyping/atomtype_processor.jl
+++ src/core/atomtyping/atomtype_processor.jl
@@ -1,4 +1,4 @@
-using BiochemicalAlgorithms
+using BiochemicalAlgorithms, DataFramesMeta, DataFrames
 
 export get_molecule_atomtypes!, count_EWG
 
@@ -10,41 +10,56 @@ function get_molecule_atomtypes!(mol::AbstractMolecule, mapfile::AbstractString)
     # loop over atoms, prefilter dataframe by element and neighbor count
     for i = (1:nrow(mol.atoms))
         def_curr_df = copy(def_file_df)
-        default_num_H_neighbors = -1
         num_H_neighbors = countmap(in(["H1"]).(atmprops_df.ElementWithNeighborCount[atmprops_df.Neighbors[i]]))[true]
-        num_EWG_groups = default_num_EWG_groups = -1
+        num_EWG_groups = -1
         if Int(mol.atoms.element[i]) == 1
-            EWG_groups = count_EWG(i, mol, atmprops_df)
-        end
-        ### filter DEF file dataframe by atom properties from preprocessing of molecule
-        # filter current dataframe def_curr_df by element number
-        if !isempty(def_curr_df[(def_curr_df[!,:atomic_number] .== Int(mol.atoms.element[i])), :])
-            df_curr = def_curr_df[(def_curr_df[!,:atomic_number] .== Int(mol.atoms.element[i])), :]
+            num_EWG_groups = count_EWG(i, mol, atmprops_df)
         end
 
-        # filter current dataframe def_curr_df by number of neighbors
-        if !isempty(df_curr[(df_curr[!,:num_neighbors] .== lastindex(atmprops_df.Neighbors[i])), :])
-            df_curr = df_curr[(df_curr[!,:num_neighbors] .== lastindex(atmprops_df.Neighbors[i])), :]
+        def_curr_df = @subset def_curr_df @byrow begin
+            :atomic_number == Int(mol.atoms.element[i])
         end
-
-        # filter current dataframe def_curr_df by number of hydrogens in direct neighborhood
-        if !isempty(df_curr[(df_curr[!,:num_H_bonds] .== num_H_neighbors), :])
-            df_curr = df_curr[(df_curr[!,:num_H_bonds] .== num_H_neighbors), :]
-        else
-            if !isempty(df_curr[(df_curr[!,:num_H_bonds] .== default_num_H_neighbors), :])
-                df_curr = df_curr[(df_curr[!,:num_H_bonds] .== default_num_H_neighbors), :]
+        df_match = false
+        while !df_match && nrow(def_curr_df) > 0
+            df_match_list = [0 for _ in 1:6]
+            for (colnum, coldata) in eachrow(def_curr_df)[1][3:8]
+                if coldata == "*" || coldata == -1
+                    df_match_list[colnum] = 1
+                    continue
+                else
+                    if colnum == 1 && coldata == Int(mol.atoms.element[i])
+                        df_match_list[colnum] = 1
+                    end
+                    if colnum == 2 && coldata == lastindex(atmprops_df.Neighbors[i])
+                        df_match_list[colnum] = 1
+                    end
+                    if colnum == 3 && coldata == num_H_neighbors
+                        df_match_list[colnum] = 1
+                    end
+                    if colnum == 4 && coldata == num_EWG_groups
+                        df_match_list[colnum] = 1
+                    end
+                    if colnum == 5 && check_BondTypes(coldata, atmprops_df[i,:])
+                        df_match_list[colnum] = 1
+                    end
+                    if colnum == 6 && CES_parser(coldata, atmprops_df[i,:])
+                        df_match_list[colnum] = 1
+                    end
+                end
             end
-        end
-
-        # filter current dataframe def_curr_df by number of electron-withdrawal groups
-        if !isempty(df_curr[(df_curr[!,:electron_withdrawal_groups] .== num_EWG_groups), :])
-            df_curr = df_curr[(df_curr[!,:electron_withdrawal_groups] .== num_EWG_groups), :]
-        else
-            if !isempty(df_curr[(df_curr[!,:electron_withdrawal_groups] .== default_num_EWG_groups), :])
-                df_curr = df_curr[(df_curr[!,:electron_withdrawal_groups] .== default_num_EWG_groups), :]
+            if all(in([1]).(df_match_list))
+                df_match = true
+                mol.atoms.atomtype[i] = def_curr_df.type_name[1]
+            else
+                mol.atoms.atomtype[i] = "DU"
             end
         end
     end
+end
+
+
+function check_BondTypes(colstring::String, atmprops_df::DataFrame)
+
 end
 
 
