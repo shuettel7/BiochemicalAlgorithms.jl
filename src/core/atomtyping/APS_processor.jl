448ea84483752e356ccd6252d61315acbedc9048
Samuel Hüttel <shuettel@students.uni-mainz.de>
2022-12-01 16:24:47 +0100

    APS_processor working with metaprogramming

--- src/core/atomtyping/APS_processor.jl
+++ src/core/atomtyping/APS_processor.jl
@@ -1,30 +1,27 @@
 # APS is the atomic_property string from the DEF file format.
 # In dataframes of imported DEF files there is a column "atomic_property"
 
+export APS_processor
 
-function APS_processor(colstring::String, atmprops_df::DataFrameRow, splitchar::Char)
+function APS_processor(colstring::String, atmprops_df::DataFrameRow)
+    colstring = colstring[2:lastindex(colstring)-1]
     and_count = count(==(','), colstring)
     or_count = count(==('.'), colstring)
     and_list = split(colstring, ',')
-    and_expr_conditionals = :(:filler)
+    and_expr_conditionals = Expr(:&&)
 
     if !all(in(atmprops_df.BondTypes).(and_list))
-        for num in 1:and_count
-            if num > 1
-                push!(and_expr_conditionals.args, :(&&), :filler)
+        for andItem in and_list
+            or_list = split(andItem, '.')
+            or_expr_conditionals = Expr(:||)
+            for orItem in or_list
+                push!(or_expr_conditionals.args, :(in($(atmprops_df.BondTypes)).($orItem)))
             end
-        end
-        for (num, item) in enumerate(and_list)
-            or_list = split(item, '.')
-            or_expr_conditionals = :(:filler)
-            for (j, orItem) in enumerate(or_list)
-                if j == 1
-                   or_expr_conditionals.args[1] = :(in(atmprops_df.BondTypes).(orItem)) 
-                else
-                    push!(or_expr_conditionals.args, :(||), :(in(atmprops_df.BondTypes).(orItem)))
-                end
+            if eval(or_expr_conditionals) == false
+                return false
+            else
+                push!(and_expr_conditionals.args, eval(or_expr_conditionals)) 
             end
-            and_expr_conditionals.args[2*(num-1)+1] = eval(or_expr_conditionals) 
         end
     else
         return true
