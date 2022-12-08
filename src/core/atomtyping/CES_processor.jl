067fbdd0d2dda9d62f021885c2bf61352f52c9a1
Samuel Hüttel <shuettel@students.uni-mainz.de>
2022-12-08 10:54:21 +0100

    CES_parser update after testing

--- src/core/atomtyping/CES_processor.jl
+++ src/core/atomtyping/CES_processor.jl
@@ -1,7 +1,9 @@
 using BiochemicalAlgorithms
 
+export CES_parser
 
-function CES_parser(colstring::String, atmprops_df::DataFrameRow, mol::AbstractMolecule, atom_num::Int)
+
+function CES_parser(colstring::String)
     
     open_chars_list = ['(', '[', '<']
     close_chars_list = [')', ']', '>']
@@ -11,49 +13,53 @@ function CES_parser(colstring::String, atmprops_df::DataFrameRow, mol::AbstractM
 
     bracket_logic_list = findall(x -> x in vcat(open_chars_list,close_chars_list,logic_chars), colstring)
 
+    
+    layers_df = DataFrame([Vector{Int}(), Vector{Int}(), Vector{String}(), Vector{Union{Int,String}}(), 
+                            Vector{String}(), Vector{String}(), Vector{Int}(), Vector{Vector{Int}}()],
+                        ["LayerId", "LayerDepth", "Element", "NumNeighbors", "ElementWithNeighborCount", 
+                            "CES_APS", "SourceRowNum", "ContainsRowNum"])
     layer = 0
-    layers_df = DataFrame([Vector{Int}(), Vector{String}(), Vector{Int}(), Vector{String}(), Vector{String}(), Vector{Vector{Int}}()],
-                    ["LayerNum", "Element", "NumNeighbors", "ElementWithNeighborCount", "CES_APS", "ContainsRowNum"])
+    layer_id = 0
+    in_APS_bool = false
     for (i,strindex) in enumerate(colstring[bracket_logic_list]) 
-        substring = colstring[bracket_logic_list[i]+1:bracket_logic_list[i+1]-1]
-        if strindex == '(' || (strindex == ',' && (colstring[bracket_logic_list[i]-1] == ')' || isnumeric(colstring[bracket_logic_list[i]-1])))
-            if strinex == '('
+        if strindex == '['
+            in_APS_bool = true
+        elseif strindex == ']' || strindex == '('
+            in_APS_bool = false
+        end
+        if strindex == '(' || (strindex == ',' && !in_APS_bool)
+            if strindex == '('
                 layer += 1
             end
-            push!(layers_df, (layer, filter(!isnumeric, substring), parse(Int, filter(isnumeric, substring)), string(Element, NumNeighbors), "", []))
+            layer_id += 1
+            substring = colstring[bracket_logic_list[i]+1:bracket_logic_list[i+1]-1]
+            curr_element = filter(!isnumeric, substring)
+            if isempty(filter(isnumeric, substring)) || contains(substring, "dd")
+                num_neighbors = ""
+                if contains(substring, "dd")
+                    curr_element = filter(!=('d'), substring)
+                end
+            else
+                num_neighbors = parse(Int, filter(isnumeric, substring))
+            end
+            push!(layers_df, (layer_id, layer, curr_element , num_neighbors, string(curr_element, num_neighbors), "", 0, []))
             if colstring[bracket_logic_list[i+1]] == '['
                 layers_df.CES_APS[nrow(layers_df)] = colstring[bracket_logic_list[i+1]:findnext(']', colstring, bracket_logic_list[i+2])]
             end 
             if layer > 1 
-                owner_layer_df = layers_df[(layers_df.LayerNum .== layer-1), :]
-                push!(owner_layer_df.ContainsRowNum[nrow(owner_layer_df)], nrow(layers_df))
+                owner_layer_df = layers_df[(layers_df.LayerDepth .== layer-1), :]
+                layers_df.SourceRowNum[nrow(layers_df)] = owner_layer_df.LayerId[nrow(owner_layer_df)]
+                push!(owner_layer_df.ContainsRowNum[nrow(owner_layer_df)], layers_df.LayerId[nrow(layers_df)])
             end
         elseif strindex == ')'
             layer -= 1
         end
     end
-
-    for rownum in 1:nrow(layers_df)
-        rowNumsChain = Vector{Int}()
-        layer_depth = 0
-        copy_layer_df = copy(layers_df)
-        while !isempty(copy_layers_df.ContainsRowNum[])
-    end
-
-    layer_expr = Expr(:&&)
-
-    # for layernum in keys(countmap(layers_df.LayerNum))
-    #     curr_layers_df = layers_df[(layers_df.LayerNum .== layernum),:]
-    #     curr_layer_atoms = filter(!(x -> x in neighborhood(mol1.properties["mol_graph"], atom_num, layernum-1)), neighborhood(mol1.properties["mol_graph"], atom_num, layernum))
-    #     for atomnum in curr_layer_atoms
-    #         push!(layer_expr.args, )
-    # end
-    
-
+    return layers_df
 end
 
 
-function CES_processor()
+function CES_processor(coldata::DataFrame, atmprops_df::DataFrame, mol::AbstractMolecule, i::Int)
     # use DEF file and information from CES_parser process to cycle through
     # DEF file until Atomtype is assigned or no more criteria fit (return error?)
 end
@@ -72,7 +78,7 @@ function CES_APS_processor(colstring::String, atmprops_df::DataFrameRow, curr_at
             or_expr_conditionals = Expr(:||)
             for orItem in or_list
                 if contains(orItem, '\'')
-                    push!(or_expr_conditionals.args, :($(enumToString(BondShortOrderType(mol.properties["weighted_graph_adj_matrix"][curr_atom, pre_atom])) == $orItem[1:lastindex(orItem)-1])))
+                    push!(or_expr_conditionals.args, :(enumToString(BondShortOrderType(mol.properties["weighted_graph_adj_matrix"][curr_atom, pre_atom]) == orItem[1:lastindex(orItem)-1])))
                 else
                     push!(or_expr_conditionals.args, :(in($(atmprops_df.BondTypes)).($orItem)))
                 end
