9b6d3501951fbb022390198ea734497e7b6c33c3
Samuel Hüttel <shuettel@students.uni-mainz.de>
2022-12-24 23:47:49 +0100

    Clearpreprocessing! func adapted with atmprops_df, tests also adapted

--- test/test_preprocessing.jl
+++ test/test_preprocessing.jl
@@ -2,7 +2,7 @@
 @testset "Preprocessing_Molecule" begin
     mol = load_pubchem_json("data/TEST_PREPROCESSING_MOLECULE_Efavirenz_Conformer3D_CID_64139.json")
     PreprocessingMolecule!(mol)
-    @test length(mol.properties) == 8
+    @test length(mol.properties) == 9
     @test length(mol.atoms.properties) == count_atoms(mol)
     @test length(mol.bonds.properties) == count_bonds(mol)
     @test length(mol.atoms.properties[20]) == 7
@@ -16,7 +16,7 @@ end
     mol = load_pubchem_json("data/TEST_PREPROCESSING_MOLECULE_Efavirenz_Conformer3D_CID_64139.json")
     mol_props_names = ["mol_graph", "adjacency_matrix", "mol_weighted_graph", 
                         "weighted_graph_adj_matrix", "chem_cycle_list", 
-                        "ring_intersections_matrix", "ring_class_list"]
+                        "ring_intersections_matrix", "ring_class_list", "atmprops_df"]
     atom_props_names = ["CycleListNum", "CycleSize", "ElementWithNeighborCount",
                         "AromaticityType", "BondTypes", "Neighbors", "SecondaryNeighbors"]
     bond_props_names = ["TRIPOS_tag"]
