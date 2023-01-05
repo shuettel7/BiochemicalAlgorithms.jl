1431d50e3d36b025432636288fe6a3068a1bf602
Samuel Hüttel <shuettel@students.uni-mainz.de>
2023-01-05 20:21:05 +0100

    postprocessing: changed path_builder to better match antechamber output

--- src/core/atomtyping/postprocessing_atomtyping.jl
+++ src/core/atomtyping/postprocessing_atomtyping.jl
@@ -8,13 +8,9 @@ function gaff_postprocessing_all_conjugated_systems!(mol::AbstractMolecule)
 
     group1_conjugated_atoms = mol.atoms[(in(group_atomtypes_dict[1]).(mol.atoms[!,:atomtype])),:]
 
-    group1_atoms_with_1_neighbor_in_group1_atoms = filter(x -> 
-                countmap(in(group1_conjugated_atoms.number).(neighbors(mol_graph, x.number)))[true] == 1, 
-                group1_conjugated_atoms)
-
     seen_atoms_vec = Vector{Int}()
     
-    for conjAtm in eachrow(group1_atoms_with_1_neighbor_in_group1_atoms)
+    for conjAtm in eachrow(group1_conjugated_atoms)
         if conjAtm.number in seen_atoms_vec
             continue
         end
@@ -24,8 +20,13 @@ function gaff_postprocessing_all_conjugated_systems!(mol::AbstractMolecule)
         depth = 1
         next_neigh_vec = neighbors_in_conjugated_atoms.number
         start_atom = conjAtm.number
-        all_paths_from_curr_conjAtm = conjAtm_path_builder!(group1_conjugated_atoms, [start_atom, next_neigh_vec[1]], mol, depth)
-        append!(seen_atoms_vec, reduce(vcat, all_paths_from_curr_conjAtm))
+        for neigh in next_neigh_vec
+            if neigh in seen_atoms_vec
+                continue
+            end
+            all_paths_from_curr_conjAtm = conjAtm_path_builder!(group1_conjugated_atoms, [start_atom, neigh], mol, depth)
+            append!(seen_atoms_vec, reduce(vcat, all_paths_from_curr_conjAtm))
+        end
     end
 end
 
@@ -42,21 +43,21 @@ function conjAtm_path_builder!(group1_conjugated_atoms::DataFrame, path_vec::Vec
     bond_in_conjugated_atoms = innerjoin(combination_df, mol.bonds, on = [:a1, :a2])
 
     if haskey(bond_in_conjugated_atoms.properties[1], "TRIPOS_tag") && bond_in_conjugated_atoms.properties[1]["TRIPOS_tag"] == "ar"
-        if mol.atoms.atomtype[previous_atom] in group_atomtypes_dict[1]
+        if mol.atoms.atomtype[previous_atom] in group_atomtypes_dict[1] && in(group_atomtypes_dict[1]).(mol.atoms.atomtype[curr_atom])
             mol.atoms.atomtype[curr_atom] = group_atomtypes_dict[2][findfirst(x -> x == mol.atoms.atomtype[curr_atom], group_atomtypes_dict[1])]
         end
     elseif bond_in_conjugated_atoms.order[1] == BondOrder.T(1)
-        if mol.atoms.atomtype[previous_atom] in group_atomtypes_dict[2]
+        if mol.atoms.atomtype[previous_atom] in group_atomtypes_dict[2] && in(group_atomtypes_dict[1]).(mol.atoms.atomtype[curr_atom])
             mol.atoms.atomtype[curr_atom] = group_atomtypes_dict[2][findfirst(x -> x == mol.atoms.atomtype[curr_atom], group_atomtypes_dict[1])]
         end
     elseif bond_in_conjugated_atoms.order[1] == BondOrder.T(2)
-        if mol.atoms.atomtype[previous_atom] in group_atomtypes_dict[1]
+        if mol.atoms.atomtype[previous_atom] in group_atomtypes_dict[1] && in(group_atomtypes_dict[1]).(mol.atoms.atomtype[curr_atom])
             mol.atoms.atomtype[curr_atom] = group_atomtypes_dict[2][findfirst(x -> x == mol.atoms.atomtype[curr_atom], group_atomtypes_dict[1])]
         end
     end
     next_neighbors_vec = filter(x -> !in(path_vec).(x) && x in neighbors(mol_graph, curr_atom) && 
                                     in(group1_conjugated_atoms.number).(x), neighborhood(mol_graph, source_atom, depth+1))
-                                    
+                    
     all_paths = Vector{Vector{Int}}()
     if isempty(next_neighbors_vec)
         push!(all_paths, path_vec)
