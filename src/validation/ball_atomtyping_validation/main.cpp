4522bcf0202bc1dd4de8a12cb50741b4c25f8422
Samuel Hüttel <shuettel@students.uni-mainz.de>
2023-02-02 16:21:25 +0100

    readapted BALLC++ script for GenericMolFactory
    Signed-off-by: Samuel Hüttel <shuettel@students.uni-mainz.de>

--- src/validation/ball_atomtyping_validation/main.cpp
+++ src/validation/ball_atomtyping_validation/main.cpp
@@ -17,8 +17,7 @@ namespace fs = std::filesystem;
 
 using namespace BALL;
 
-int main() {
-	//int argc, char** argv
+int main() {	//int argc, char** argv
 
 	// if (argc != 2)
 	// {
@@ -26,82 +25,64 @@ int main() {
 	// 	return(1);
 	// }
 
-	const fs::path path {"mnt/c/Users/Sam/source/repos/huettel-msc/gaff_files/"};
+	const fs::path path {"/mnt/c/Users/Sam/source/repos/huettel-msc/gaff_files"};
 	std::cout << path << typeid(path).name() << std::endl;
 	
-	for (const auto& entry : fs::directory_iterator{path}){
-		std::cout << entry.path() << std::endl;}
-	// 	std::string entry_path = entry.path();
-	// 	std::string hello_world = "hello_world";
-	// 	if (typeid(entry_path).name() != typeid(hello_world).name())
-	// 	{
-	// 		Log.error() << "filetype1: " << typeid(path).name() << std::endl;
-	// 		return 2;
-	// 	} else if (entry_path.find(".mol2") != string::npos)
-	// 	{
-	// 		Log.error() << "entry_path.find(): " << entry_path << std::endl;
-	// 	}
+	for (const auto & entry : fs::directory_iterator{path}){
 
-	// 	if (entry_path.find(".mol2") == string::npos){
-	// 		continue;
-	// 	}
+		std::string entry_path {entry.path()};
 		
-	// 	if (typeid(entry_path).name() != typeid("hello world").name())
-	// 	{
-	// 		Log.error() << "filetype2: " << typeid(entry_path).name() << std::endl;
-	// 		return 2;
-	// 	}
+		if (entry_path.find(".mol2") == string::npos){
+			continue;
+		}
 
-	// 	GenericMolFile* infile = MolFileFactory::open(entry_path);
+		GenericMolFile* infile = MolFileFactory::open(entry_path);
 
-	// 	if (!infile)
-	// 	{
-	// 		Log.error() << "Could not determine filetype, aborting" << std::endl;
-	// 		return 2;
-	// 	}
+		if (!infile)
+		{
+			Log.error() << "Could not determine filetype, aborting" << std::endl;
+			return 2;
+		}
 
-	// 	if (!*infile)
-	// 	{
-	// 		std::cerr << "Invalid file, aborting" << std::endl;
-	// 		return 2;
-	// 	}
+		if (!*infile)
+		{
+			std::cerr << "Invalid file, aborting" << std::endl;
+			return 2;
+		}
 		
-	// 	std::string infile_name = entry_path; 
-	// 	// ("../../../../../huettel-msc/export_folder/gaff_files/1_1,3-Butadien_Conformer3D_CID_7845.mol2"); 
-	// 	std::string basename_infile = entry_path.substr(entry_path.find_last_of("/\\") + 1);
-	// 	std::string outfile_name = "/mnt/c/Users/Sam/source/repos/huettel-msc/gaff_files/ballC_output/" + basename_infile;
+		std::string infile_name = entry_path; 
+		// ("../../../../../huettel-msc/export_folder/gaff_files/1_1,3-Butadien_Conformer3D_CID_7845.mol2"); 
+		std::string basename_infile = entry_path.substr(entry_path.find_last_of("/\\") + 1);
+		std::string outfile_name = "/mnt/c/Users/Sam/source/repos/huettel-msc/gaff_files/ballC_output/" + basename_infile;
 
-	// 	// MOL2File f(infile_name);
-	// 	System S;
-	// 	*infile >> S;
+		// MOL2File f(infile_name);
+		System S;
+		*infile >> S;
 
-	// 	// Important: Cleanup
-	// 	infile->close();
-	// 	delete infile;
+		// Important: Cleanup
+		infile->close();
+		delete infile;
 
-	// 	Options options;
-	// 	options[GAFFTypeProcessor::Option::ATOMTYPE_FILENAME] = "atomtyping/GAFFTypes.dat";
+		Options options;
+		options[GAFFTypeProcessor::Option::ATOMTYPE_FILENAME] = "atomtyping/GAFFTypes.dat";
 
-	// 	GAFFTypeProcessor gt(options);
-	// 	S.apply(gt);
+		GAFFTypeProcessor gt(options);
+		S.apply(gt);
 
-	// 	for (AtomIterator at_it = S.beginAtom(); +at_it; ++at_it)
-	// 		std::cout << "atom name: " << at_it->getName() << " atomtype  " << at_it->getProperty("atomtype").getString() << std::endl;
+		for (AtomIterator at_it = S.beginAtom(); +at_it; ++at_it)
+			std::cout << "atom name: " << at_it->getName() << " atomtype  " << at_it->getProperty("atomtype").getString() << std::endl;
 
-	// 	AssignBondOrderProcessor abp;
-	// 	S.apply(abp);
+		AssignBondOrderProcessor abp;
+		S.apply(abp);
 
-	// 	std::cout << "System contains " << S.countAtoms() << " atoms." << std::endl;
+		std::cout << "System contains " << S.countAtoms() << " atoms." << std::endl;
 		
-	// 	GenericMolFile* outfile = MolFileFactory::open(outfile_name, std::ios::out);
-	// 	*outfile << S;
-	// 	outfile->close();
-	// 	delete outfile;
-	// 	// MOL2File mol2_file(outfile_name, ios::out);
-	// 	// mol2_file << S;
-	// 	// mol2_file.close();
-	// 	// cout << "Wrote MOL2 file " << outfile_name << endl;
-	// }
+		GenericMolFile* outfile = MolFileFactory::open(outfile_name, std::ios::out);
+		*outfile << S;
+		outfile->close();
+		delete outfile;
+		std::cout << "Wrote MOL2 file " << outfile_name << std::endl;
+	}
 
 	return 0;	
 }
